981f89be7f395b86ba05950c6972b6fdac993f3f
David F. Severski <davidski@deadheaven.com>
2017-11-26 08:28:02 -0800

    Add donation encouragement on package attach.

--- R/hatetracker.R
+++ R/hatetracker.R
@@ -9,3 +9,8 @@
 #' @docType package
 #' @name hatetracker
 NULL
+
+.onAttach <- function(...) {
+  packageStartupMessage("Hatetracker.IO is a service from the Southern Poverty Law Center.")
+  packageStartupMessage("Please consider donating at https://donate.splcenter.org/sslpage.aspx?pid=463")
+}
